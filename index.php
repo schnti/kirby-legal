60f37186313da5524a9bf76a7550da410decb3ab
Timo Schneider <tschneider@spirit21.de>
2019-04-03 11:55:57 +0200

    Init.

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+Kirby::plugin('schnti/legal', [
+	'options' => [
+		'username'   => false,
+		'password'   => false,
+		'data'       => [],
+		'version'    => 'newest',
+		'cache.data' => true
+	],
+	'tags'    => [
+		'verantwortlicheStelle'      => [
+			'html' => function ($tag) {
+				$page = $tag->parent();
+				return $page->verantwortlicheStelle()->kirbytext();
+			}
+		], 'datenschutzbeauftragter' => [
+			'html' => function ($tag) {
+				$page = $tag->parent();
+				return $page->datenschutzbeauftragter()->kirbytext();
+			}
+		], 'legal'                   => [
+			'attr' => [
+				'class'
+			],
+			'html' => function ($tag) {
+
+				$resource = $tag->value;
+
+				$apiCache = kirby()->cache('schnti.legal.data');
+				$apiData = $apiCache->get($resource);
+
+				if ($apiData === null) {
+
+					$response = Kirby\Http\Remote::post('https://legal-api.kleiner-als.de/rest/content/' . $resource, [
+						'headers' => [
+							'Authorization: Basic ' . base64_encode(option('schnti.legal.username') . ':' . option('schnti.legal.password'))
+						],
+						'data'    => option('schnti.legal.data'),
+						'version' => option('schnti.legal.version'),
+					]);
+
+					$apiCache->set($resource, $response->content());
+
+				}
+
+				return kirbytext($apiData);
+			}
+		]
+	]
+]);
